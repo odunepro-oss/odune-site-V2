3311aca212760143d00344cb3a3899eea35f62d1
odunepro-oss <odunepro@gmail.com>
2026-09-03 17:39:39 +0200

    Favicon branché sur toutes les pages

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,6 +10,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/icone-tuile.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Geist+Mono:wght@300;400;500&family=Geist:wght@400&display=swap" />
